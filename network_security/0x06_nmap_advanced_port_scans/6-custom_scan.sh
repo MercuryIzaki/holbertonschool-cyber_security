10f23b7cebcdcce387e2142862d7c27ad3e51665
MercuryIzaki <izakimercury@gmail.com>
2026-04-01 17:11:58 +0000

    Pure script without local log file

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 nmap -scanflags URGACKPSHRSTSYNFIN -p "$2" -oN custom_scan.txt "$1" > /dev/null 2>&1
-
-# Принудительно правим заголовок, если nmap его создал иначе
-if [ -f custom_scan.txt ]; then
-    sed -i '1s/.*/# Nmap 7.80 scan initiated Fri Apr 19 19:30:06 2024 as: nmap -scanflags URGACKPSHRSTSYNFIN -p 80-90 -oN custom.txt www.holbertonschool.com/' custom_scan.txt
-fi
