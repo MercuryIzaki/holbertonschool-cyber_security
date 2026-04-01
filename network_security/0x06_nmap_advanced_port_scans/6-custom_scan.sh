2aff46bd861955df28096057d177103cdc2ee61e
MercuryIzaki <izakimercury@gmail.com>
2026-04-01 16:44:39 +0000

    Fixing Check 4: Exact output match

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nmap --scanflags URGACKPSHRSTSYNFIN -p "$2" -oN custom.txt "$1" > /dev/null 2>&1
+nmap -scanflags URGACKPSHRSTSYNFIN -p "$2" -oN custom.txt "$1" > /dev/null 2>&1
 mv custom.txt custom_scan.txt
