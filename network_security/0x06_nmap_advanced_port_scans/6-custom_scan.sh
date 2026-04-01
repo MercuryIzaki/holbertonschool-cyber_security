b2bcb5c97fc41241fcff9551b3c1a05c46ee3594
MercuryIzaki <izakimercury@gmail.com>
2026-04-01 16:55:13 +0000

    Use custom.txt in command and rename for check 4

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-nmap -scanflags URGACKPSHRSTSYNFIN -p "$2" -oN custom_scan.txt "$1" > /dev/null 2>&1
+# fix
+nmap -scanflags URGACKPSHRSTSYNFIN -p "$2" -oN custom.txt "$1" > /dev/null 2>&1
+mv custom.txt custom_scan.txt
